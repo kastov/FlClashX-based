ef448dc0564dcf26c60ca3e8f67901eb11dbde40
pluralplay <vayudanov@ya.ru>
2025-07-23 21:57:10 +0300

    some changes

--- services/helper/target/release/deps/helper.d
+++ services/helper/target/release/deps/helper.d
@@ -7,4 +7,4 @@ src\service\mod.rs:
 src\service\hub.rs:
 src\service\windows.rs:
 
-# env-dep:TOKEN=29d09ab5eb501b5ebbe1be94ea53e8a6efa17b43cc8de592a50f2f3a8a73ada7
+# env-dep:TOKEN=268f2a63114ef928c1851bc6712269a6f9d177110213605d7dca7b1fb5e3101f
